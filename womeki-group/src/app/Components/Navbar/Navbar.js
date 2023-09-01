b7fa65b113cce856f9cb6903cf17e11c7f3a30f9
“Atif0905” <atifnaquee09@gmail.com>
2023-09-01 12:35:54 +0530

    Innitial-Setup

---
+++ womeki-group/src/app/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="H1tag">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ womeki-group/src/app/page.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from './Components/Navbar/Navbar'
+import React from 'react'
+import "./page.css";
+
+export default function Home() {
+  return (
+  <div>
+  <Navbar/>
+  </div>
+  )
+}
