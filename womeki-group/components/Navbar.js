1c69a1e41de0d6675de9e0de1d228519ee0b3d0c
“Atif0905” <atifnaquee09@gmail.com>
2023-09-04 15:11:41 +0530

    Changes

--- womeki-group/components/Navbar.js
+++ womeki-group/components/Navbar.js
@@ -8,6 +8,7 @@ const MENU_LIST = [
   { text: "Home", href: "/" },
   { text: "About Us", href: "/about" },
   { text: "Contact", href: "/contact" },
+  {text: "Career" , href: "/Career"}
 ];
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
