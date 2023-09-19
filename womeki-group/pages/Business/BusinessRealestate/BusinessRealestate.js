a6362e216abfc058348cd343a974eeb35e3ad72c
Kirti <kirti.azby@gmail.com>
2023-09-19 11:12:24 +0530

    newbusiness

---
+++ womeki-group/pages/Business/BusinessRealestate/BusinessRealestate.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import data from "./BusinessRealestate.json";
+import styles from "./BusinessRealestate.module.css";
+import Image from "next/Image";
+
+const BusinessRealestate = () => {
+  return (
+    <div className='container'>
+         <div className='row bg-white'>
+            {data.RealEstate.map((NewData) => {
+                return(
+                    <div className={`col-lg-4 col-md-6 mt-3`}>
+                    
+                      <div className={`card border-0 rounded-0 ${styles.businesscardbox}`}>
+                        <Image className={`card-img-top ${styles.businesscardimg}`} width={500} height={250}  src={NewData.Image}/>
+                        <div className='card-body'>
+                        <h5 className="card-title">{NewData.Heading}</h5>
+                        <p className="card-text">{NewData.Content}</p>
+                        </div>
+                      </div>
+                      
+                    </div>
+                )
+            })}
+           
+        </div>
+      
+    </div>
+  )
+}
+
+export default BusinessRealestate
