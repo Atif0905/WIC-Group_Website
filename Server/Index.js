85e8f8779b01fa08bba6200f38ba7a40f6936965
“Atif0905” <atifnaquee09@gmail.com>
2023-09-20 18:15:46 +0530

    Uploads done

--- Server/Index.js
+++ Server/Index.js
@@ -4,7 +4,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose")
+const Post = require('./models/Post')
 const User = require("./models/User")
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs')
+
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
@@ -14,11 +19,13 @@ app.use(cors({credentials:true,origin:'http://localhost:3000'}))
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect("mongodb+srv://Womeki:bbZWMWIhMDWQ5bi2@cluster0.f5ozdcn.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true, // Enable SSL/TLS
+  ssl: true,
 });
 
 app.post('/register', async (req, res) => {
@@ -51,6 +58,41 @@ app.post('/login', async (req,res) => {
     res.status(400).json('wrong credentials');
   }
 });
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'),async (req,res) =>{
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext
+  fs.renameSync(path,newPath )
+
+  const {title,summary, content} = req.body;
+   
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover:newPath,
+  })
+  console.log(postDoc)
+
+
+ res.json(postDoc)
+})
+
+app.get('/post', async(req, res) =>{
+  res.json(await Post.find());
+})
 app.listen(4000);
 
 // mongo pass = bbZWMWIhMDWQ5bi2
---
+++ womeki-group/pages/Amin/Header.js
@@ -0,0 +1,34 @@
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
+import Link from 'next/link'
+
+const Header = () => {
+    const {setUserInfo,userInfo} = useContext(UserContext);
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+          credentials: 'include',
+        }).then(response => {
+          response.json().then(userInfo => {
+            // setUserInfo(userInfo);
+          });
+        });
+      }, []);
+      function logout() {
+        fetch('http://localhost:4000/logout', {
+          credentials: 'include',
+          method: 'POST',
+        });
+        setUserInfo(null);
+      }
+  return (
+    <div >
+        <div  className=' pt-5 header-backend ' >
+        <Link href="./AdminLogin">Login</Link>
+            <Link href="./AdminUser">Register</Link>
+            <a onClick={logout} >Log-Out</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
