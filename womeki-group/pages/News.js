659a46415daadbd118ea02acae32ee043c4143a1
Kirti <kirti.azby@gmail.com>
2023-09-21 16:20:11 +0530

    newsapidesign

--- womeki-group/pages/News.js
+++ womeki-group/pages/News.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import News from './News/News'
+import styles from "../pages/News/News.module.css";
 
 const news = () => {
   const [posts,setPosts] = useState([]);
@@ -13,6 +14,9 @@ const news = () => {
   }, []);
   return (
     <div className="pt-5">
+       <h1 className={styles.newswomekihead} >Womeki</h1>
+       <h4 className={` ${styles.newswomekisubhead}`}>Group's insider scoop - Your passport to insights</h4>
+       <h1 className={` text-center ${styles.newsmaintitle}`}>News</h1>
     {posts.length > 0 ? (
       posts.map((newPost, index) => (
         <News key={index} {...newPost} />
--- womeki-group/pages/News/News.js
+++ womeki-group/pages/News/News.js
@@ -2,19 +2,25 @@ import React from "react";
 import styles from "./News.module.css";
 import data from "./News.json";
 
+
+
+      
+
 const News = ({ title, cover, summary, createdAt, content }) => {
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="row">
-        <div>
-          <img src={"http://localhost:4000/" + cover} alt="nn" />
-          <h1 className={styles.newstitle}>{title}</h1>
-          <h1>{summary}</h1>
-          <h1>{createdAt}</h1>
-          <p>{content}</p>
+          <div className="col-lg-4 col-md-6 col-sm-12">
+          <img className={styles.newsimg}  height={300} width={300} src={"http://localhost:4000/" + cover} alt="nn" />
+          </div>
+          <div className="col-lg-8 col-md-6 col-sm-12">
+          <h2 className={styles.newshead}>{title}</h2>
+          <p className={styles.newscontent}>{summary}</p>
+          <p className={`mt-5 ${styles.newspara}`}>{createdAt}</p>
+          </div>
+          {/* <p>{content}</p> */}
         </div>
       </div>
-    </div>
   );
 };
 
