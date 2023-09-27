23db2f710e310490ee2662f796acab71c8498147
“Atif0905” <atifnaquee09@gmail.com>
2023-09-27 12:25:44 +0530

    Homepage-News

--- womeki-group/pages/index.js
+++ womeki-group/pages/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import Carousel from "./HomePage/Carousel/Carousel";
 import Aboutcard from "./HomePage/AboutCard/Aboutcard";
 import Buisness_Card from './HomePage/Buisness/Buisness_Card'
@@ -8,13 +9,29 @@ import OurCommitment from "./HomePage/OurCommitment/OurCommitment";
 import Sustainability2 from "./HomePage/Sustainability2/Sustainability2";
 
  const  Home = () => {
+  const [posts,setPosts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/post').then(response => {
+      response.json().then(posts => {
+        setPosts(posts);
+        console.log(posts)
+      });
+    });
+  }, []);
   return (
     <div>
      <Carousel/>
      <Buisness_Card/>
      <Aboutcard/>
      <HomeBlog/>
-     <NewsCard/>
+     {posts.length > 0 ? (
+      posts.map((newPost, index) => (
+        <NewsCard key={index} {...newPost} />
+      ))
+    ) : (
+      <p>No posts available.</p>
+    )}
+     {/* <NewsCard/> */}
      <Sustainability/>
      <OurCommitment/>
      {/* <Sustainability2/> */}
