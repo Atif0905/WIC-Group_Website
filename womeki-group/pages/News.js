880cbee7aa4c5ef41ed54cf43fc76f84bf4cca53
Akkisharma87 <asakshay.982000@gmail.com>
2023-09-29 18:26:35 +0530

    done

--- womeki-group/pages/News.js
+++ womeki-group/pages/News.js
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import News from './News/News'; // Assuming 'News' is another component you've imported
-
+import React, { useEffect, useState } from "react";
+import News from "./News/News";
 import styles from "../pages/News/News.module.css";
 
 const NewsPage = () => {
   const [posts, setPosts] = useState([]);
-  
+
   useEffect(() => {
-    fetch('http://localhost:4000/post').then(response => {
-      response.json().then(posts => {
+    fetch("http://localhost:4000/post").then((response) => {
+      response.json().then((posts) => {
         setPosts(posts);
-        console.log(posts)
+        console.log(posts);
       });
     });
   }, []);
 
   return (
     <div className="pt-5">
-      <h1 className={styles.newswomekihead}>Womeki</h1>
-      <h4 className={`${styles.newswomekisubhead}`}>Groups insider scoop Your passport to insights</h4>
+      <div className={`d-flex ms-5 ${styles.responsivediv}`}>
+        <div className={` text-center ${styles.newswomekihead}`}>
+          <h1 className={` text-center ${styles.newswomekiheading}`}>Womeki Times</h1>
+        </div>
+        <div className={`text-center ${styles.borderDiv}`}>
+          <span className={styles.hiddenText}>aaaaaaaaaaaaaaaaaa</span>
+        </div>
+      </div>
+      {/* <h4 className={`${styles.newswomekisubhead}`}>
+        Groups insider scoop Your passport to insights
+      </h4> */}
       <h1 className={`text-center ${styles.newsmaintitle}`}>News</h1>
       {posts.length > 0 ? (
-        posts.map((newPost, index) => (
-          <News key={index} {...newPost} />
-        ))
+        posts.map((newPost, index) => <News key={index} {...newPost} />)
       ) : (
         <p>No posts available.</p>
       )}
+      <div className="d-flex ms-5 mt-5">
+        <div className={`text-center ${styles.borderDiv1}`}>
+          <span className={styles.hiddenText1}>aaaaaaaaaaaaaaaaaa</span>
+        </div>
+        <div className={` text-center ${styles.newswomekihead1}`}>
+          <span>
+            <h1 className={` text-center ${styles.newswomekiheading}`}>
+              Womeki
+            </h1>
+          </span>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default NewsPage;
--- womeki-group/pages/News/News.js
+++ womeki-group/pages/News/News.js
@@ -1,23 +1,33 @@
 import React from "react";
 import styles from "./News.module.css";
-import Image from 'next/image'
+import Image from "next/image";
 
-const   News = ({ title, cover, summary, createdAt, content }) => {
+const News = ({ title, cover, summary, createdAt, content }) => {
   return (
-    <div className={`container mt-5 ${styles.maindiv}`}>
-      <div className="row">
-          <div className="col-lg-4 col-md-6 col-sm-12">
-          <picture>
-          <Image className={styles.newsimg} height={300} width={300} src={"http://localhost:4000/" + cover} alt="nn" />
-          </picture>
-          </div>
-          <div className="col-lg-8 col-md-6 col-sm-12">
-          <h2 className={styles.newshead}>{title}</h2>
-          <p className={styles.newscontent}>{summary}</p>
-          <p className={`mt-5 ${styles.newspara}`}>{createdAt}</p>
+    <div >
+    <div className="fluid">
+      <div className={`container mt-5 ${styles.maindiv}`}>
+        <div className="row">
+          <div className="col-12 text-center">
+            <div>
+            <Image
+              className={styles.newsimg}
+              height={400}
+              width={800}
+              src={"http://localhost:4000/" + cover}
+              alt="nn"
+            />
+            <div className={styles.overDiv}>
+            <h4 className={ styles.newshead}>{title}</h4>
+            <p className={styles.newscontent}>{summary}</p>
+            <p className={`mt-5 ${styles.newspara}`}>{createdAt}</p>
+            </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
+    </div>
   );
 };
 export default News;
